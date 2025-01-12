f1f8f0cf8d8d3e30d9a5baf76c8a8817860757b2
Jay Gurnani <jay.gurnani@gmail.com>
2025-01-12 22:50:41 +1100

    Set

--- src/main.java
+++ src/main.java
@@ -40,9 +40,11 @@ public class main {
 //        var output = earliestAcqFixed(logs, 6);
 
 
-        int[] input1 = new int[] {2,1,3,3,3,0,0,10};
-        int output = thirdDistinctTree(input1);
-        System.out.println(output == 1);
+        //int[] input1 = new int[] {2,1,3,3,3,0,0,10};
+        //int output = thirdDistinctTree(input1);
+
+        boolean output = closeStrings("abbzzca", "babzzcz");
+        System.out.println(output );
     }
 
     public static List<String> generatePalindromes(String input) {
@@ -465,6 +467,30 @@ public class main {
 
         return distinctNums.first();
     }
+
+    public static boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> word1Map = new HashMap<>();
+        Map<Character, Integer> word2Map = new HashMap<>();
+        for (char c : word1.toCharArray()) {
+            word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : word2.toCharArray()) {
+            word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
+        }
+        if (!word1Map.keySet().equals(word2Map.keySet())) {
+            return false;
+        }
+        List<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
+        List<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
+        Collections.sort(word1FrequencyList);
+        Collections.sort(word2FrequencyList);
+        return word1FrequencyList.equals(word2FrequencyList);
+    }
+
 
     public class TreeNode {
         int val;
